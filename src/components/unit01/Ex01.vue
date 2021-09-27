de4e00f783c704c7efaaafdfd91a1678c331127a
htdao <htdao00@gmail.com>
2021-09-27 15:57:56 +0700

    first commit

---
+++ src/components/unit01/Ex01.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 500px;
+}
+</style>
