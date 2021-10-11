4ad77a9c5598c05e1115b657114e91f61a16d89f
htdao <htdao00@gmail.com>
2021-10-11 11:49:50 +0700

    unit05

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,44 @@
 <template>
   <div id="app">
-<!--    <img alt="Vue logo" src="./assets/logo.png">-->
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-<!--    <Ex01/>-->
-<!--    <Ex02/>-->
-<!--    <Page1/>-->
-<!--    <Page2/>-->
-<!--    <Exercise/>-->
-<!--    <Ex2_1/>-->
-<!--    <Ex2_2/>-->
-<!--    <BaseLayout/>-->
-<!--    <ProductPage/>-->
-<!--    <Todo/>-->
-    <NewsDetailPage/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link :to="`/users/${userId}`">User</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-// import Ex01 from './components/unit01/Ex01.vue'
-// import Ex02 from './components/unit01/Ex02.vue'
-// import Ex01 from './components/unit02/Ex01.vue'
-// import Ex01 from './components/unit02/Ex01.vue'
-// import Ex01 from './components/Unit03/Ex01.vue'
-// import Page1 from './components/Unit04/Page1.vue'
-// import Page2 from "./components/Unit04/Page2.vue";
-// import Exercise from "./components/Unit04/HW/Ex3_2/Exercise.vue";
-// import Ex2_1 from "./components/Unit04/HW/Ex2_1/Ex2_1";
-// import Ex2_2 from "./components/Unit04/HW/Ex2_2/Ex2_2";
-// import BaseLayout from "./components/Unit04/HW/Ex4_1/BaseLayout";
-// import ProductPage from "./components/Unit04/HW/Ex4_1/ProductPage";
-// import Todo from "./components/Unit04/HW/Ex3_1/Todo";
-// import BaseLayout from "./components/Unit04/HW/Ex4_2/BaseLayout";
-import NewsDetailPage from "./components/Unit04/HW/Ex4_2/NewDetailPage";
 export default {
-  name: 'App',
-  components: {
-    NewsDetailPage,
-    // Todo
-    // ProductPage,
-    // HelloWorld
-    // Ex01
-    // Ex02
-    // Page1
-    // Page2
-    // Exercise
-    // Ex2_1
-    // Ex2_2
-    // BaseLayout
+  data() {
+    return {
+      userId: 5
+    }
+  },
+
+  components:{
   }
 }
 </script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
+  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/views/User.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an user page</h1>
+    <button @click="toProfilePage">Profile</button>
+    <button @click="toPostPage">Post</button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  methods:{
+    toProfilePage(){
+      this.$router.push({ name: 'profile', params: { name: 'Đào' }})
+    },
+    toPostPage(){
+      this.$router.push({ name: 'post', params: { name: 'Đào' }})
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
